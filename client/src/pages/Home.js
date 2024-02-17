5f0487f4c2de965eb98fe73f7281c281dbdfd42a
AhmedElmoslmany <100316692+Ahmed-Elmoslmany@users.noreply.github.com>
2024-02-17 15:11:28 +0200

    Update api url Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,12 +9,15 @@ import LinearProgress from '@mui/material/LinearProgress';
 function Home() {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const baseUrl = "https://blog-server-alpha-blue.vercel.app"
+  
   useEffect(() => {
     async function getArticles() {
       try {
         setIsLoading(true)
         
-        const res = await axios.get("http://127.0.0.1:5000/api/v1/articles?fields=metaData&limit=4&sort=createdAt");
+        const res = await axios.get(`${baseUrl}/api/v1/articles?fields=metaData&limit=4&sort=createdAt`);
         const data = res.data;
         setArticles(data.data.articles);
         setIsLoading(false)
