357733caf69c4a9507431593ee00a2962b6d005c
Ahmed-Elmoslmany <ahmedelmoslmany74@gmail.com>
2024-02-17 17:02:35 +0200

    add web analysis

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -9,7 +11,8 @@ root.render(
 
 
     <App />
-  
+    <Analytics />
+
  
   </React.StrictMode>
 );
