da834eddcf3a2a1bd21c15c088f2b6c499fcb349
Ahmed-Elmoslmany <ahmedelmoslmany74@gmail.com>
2024-02-22 20:21:30 +0200

    article 2

--- client/codeFormatter.js
+++ client/codeFormatter.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const relaceLinesWithBackSlashes= () => {
     // let text = "example1; example2; example3";
-    let text = fs.readFileSync('./src/markdown/article.md', 'utf-8');
+    let text = fs.readFileSync('./src/markdown/article2.md', 'utf-8');
 
     // Using regular expression to replace colon with newline
     // text = text.replace(/;/g, '\\n');
@@ -9,6 +9,8 @@ const relaceLinesWithBackSlashes= () => {
     // text = text.replace(/"/g, '\'');
     text = text.replace(/```js([\s\S]+?)```/g, '<Code language=\'js\'>$1</Code>');
     text = text.replace(/```bash([\s\S]+?)```/g, '<Code language=\'bash\'>$1</Code>');
+    text = text.replace(/%(\w+)([\s\S]+?)%/g, '<span className=\'$1\'>$2</span>');
+
     text = text.replace(/`([\s\S]+?)`/g, '<span className=\'highlighted-text\'>$1</span>');
     text = text.replace(/~([\s\S]+?)~/g, '<div className=\'important-note\'>$1</div>');
 
